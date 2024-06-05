b5e44fdaad2441249095ae7ddb84221e90a2773a
José Quintino <josequintinno@icloud.com>
2024-06-05 20:02:49 -0300

    :small_blue_diamond: feature: Implementar servicoes de persistência de Acessos

---
+++ src/main/java/br/com/quintinnodigital/DEFESIOAPI/controller/ChaveSegurancaController.java
@@ -0,0 +1,11 @@
+package br.com.quintinnodigital.DEFESIOAPI.controller;
+
+import br.com.quintinnodigital.DEFESIOAPI.dto.ChaveSegurancaRequestDTO;
+
+public class ChaveSegurancaController {
+	
+	public ChaveSegurancaRequestDTO persistir(ChaveSegurancaRequestDTO chaveSegurancaRequestDTO) {
+		return chaveSegurancaRequestDTO;
+	}
+	
+}
