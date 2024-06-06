4dae5789618d01883c1a1df4e5c4dcfab0914b54
José Quintino <josequintinno@icloud.com>
2024-06-05 21:37:10 -0300

    :small_blue_diamond: feature: Implementar consulta aos dados basicos (pessoa e categoria acessos)

---
+++ src/main/java/br/com/quintinnodigital/DEFESIOAPI/controller/PessoaController.java
@@ -0,0 +1,25 @@
+package br.com.quintinnodigital.DEFESIOAPI.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.quintinnodigital.DEFESIOAPI.entity.PessoaEntity;
+import br.com.quintinnodigital.DEFESIOAPI.service.PessoaService;
+
+@RestController
+@RequestMapping("/api/v1/pessoa")
+public class PessoaController {
+	
+	@Autowired
+	private PessoaService pessoaService;
+	
+	@GetMapping("/")
+	public List<PessoaEntity> findAll() {
+		return this.pessoaService.finAll();
+	}
+
+}
---
+++ src/main/java/br/com/quintinnodigital/DEFESIOAPI/service/PessoaService.java
@@ -0,0 +1,21 @@
+package br.com.quintinnodigital.DEFESIOAPI.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.quintinnodigital.DEFESIOAPI.entity.PessoaEntity;
+import br.com.quintinnodigital.DEFESIOAPI.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public List<PessoaEntity> finAll() {
+		return this.pessoaRepository.findAll();
+	}
+
+}
